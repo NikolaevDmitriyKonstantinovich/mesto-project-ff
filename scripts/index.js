adf4ffd4fb82d04f6516f8d3057d5603f38a478e
Dmitriy Nikolaev <frcndk@gmail.com>
2024-02-19 22:44:25 +0500

    F

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+//find template and place for cards
+const cardTemplate = document.querySelector('#card-template').content;
+const placeCards = document.querySelector('.places__list');
+
+
+//implement function add
+function addCards(name, link, deleteCardFunction) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode('true');
+    const cardDesc = cardElement.querySelector('.card__description');
+
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = 'Пейзаж ${name}';
+    cardDesc.querySelector('.card__title').textContent = name;
+
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCardFunction);
+    return cardElement;
+}
+
+//create function for delete card
+const deleteCard = (evt) => {
+    evt.target.parentElement.remove();
+};
+
+//add all cards from cards.js
+for(let i = 0; i < initialCards.length; i++) {
+    placeCards.append(addCards(initialCards[i].name, initialCards[i].link, deleteCard))
+};
